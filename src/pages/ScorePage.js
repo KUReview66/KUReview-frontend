635a6e4b34316d1ee497c738c4397419998133d6
PloyyNK <nartnatta_kri@yahoo.com>
2025-01-11 03:27:23 +0700

    lesson overview panel

--- src/pages/ScorePage.js
+++ src/pages/ScorePage.js
@@ -1,9 +1,10 @@
+import ContentPanel from "../components/ContentPanel";
 import Navbar from "../components/navBar";
 import ScoreBox from "../components/scoreBox";
 
 export default function ScorePage() {
 
-    let courseName = '012XXXXX Computer Programming I';
+    let courseName = '012XXXXX Computer Programming';
     let score = [
         {
             round: 1,
@@ -20,22 +21,33 @@ export default function ScorePage() {
             condition: 15, 
             list: 12, 
             function: 15
+        },
+        {
+            round: 3,
+            total: 75, 
+            loop: 18, 
+            condition: 18, 
+            list: 19, 
+            function: 20
         }
     ]
 
     return (
         <>
         <Navbar />
-        <div className="full-page" >
-            <div className="score-container" style={{marginLeft: '2rem'}}>
+        <div className="full-page" style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div className="score-container" style={{marginLeft: '2rem', width: '100%'}}>
                 <div className="header">
                     <h3>{courseName}</h3>
                 </div>
-                <div className="score-box">
+                <div className="score-box" style={{paddingRight: '2rem'}}>
                     {score.map((item => (
                         <ScoreBox round={item.round} score={item}></ScoreBox>
                     )))}
                 </div>
+            </div>
+            <div className="right-panel" style={{width: '100%', marginTop: '2rem'}}>
+                    <ContentPanel />
             </div>
         </div>
         </>
