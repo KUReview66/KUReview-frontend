cb4c1c2a7b07b987550c41db1bcb7a392dfca4e3
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2025-03-25 03:39:26 +0700

    update nav bar with 7 exercises

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import LogInPage from './pages/LoginPage';
 import ScorePage from './pages/ScorePage'; 
 import NotFound from "./pages/NotFound";
 import PreExamSuggest from "./pages/PreExamSuggest";
+import ExerciseU2Page from "./pages/ExerciseU2Page"
+import ExerciseU3Page from "./pages/ExerciseU3Page";
+import ExerciseU5Page from "./pages/ExerciseU5Page";
+import ExerciseU6Page from "./pages/ExerciseU6Page";
+import ExerciseU7Page from "./pages/ExerciseU7Page";
+import ExerciseU8Page from "./pages/ExerciseU8Page";
+import ExerciseU9Page from "./pages/ExerciseU9Page";
+
 
 export default function App() {
   return (
@@ -18,6 +26,16 @@ export default function App() {
         <Route path="/score/:username" element={<ScorePage />} /> 
         <Route path="/404" element={<NotFound />}/>
         <Route path="*" element={<NotFound  />} />
+        <Route path="/exerciseU2" element={<ExerciseU2Page  />} />
+        <Route path="/exerciseU3" element={<ExerciseU3Page  />} />
+        <Route path="/exerciseU5" element={<ExerciseU5Page  />} />
+        <Route path="/exerciseU6" element={<ExerciseU6Page  />} />
+        <Route path="/exerciseU7" element={<ExerciseU7Page  />} />
+        <Route path="/exerciseU8" element={<ExerciseU8Page  />} />
+        <Route path="/exerciseU9" element={<ExerciseU9Page  />} />
+
+
+
       </Routes>
     </BrowserRouter>
   );
